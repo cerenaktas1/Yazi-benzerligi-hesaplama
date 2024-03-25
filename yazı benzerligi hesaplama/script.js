b5e8cc7e78278e07e8ea6a307505d86cf6ca6e0a
cerenaktas1 <159536442+cerenaktas1@users.noreply.github.com>
2024-03-25 18:34:06 +0300

    Add files via upload

---
+++ yazı benzerligi hesaplama/script.js
@@ -0,0 +1,39 @@
+const text1Input = document.getElementById("text1");
+const text2Input = document.getElementById("text2");
+const resultElement = document.getElementById("result");
+const calculateButton = document.getElementById("calculateButton");
+
+//Benzerlik ölçme 
+
+let calculateSimilarity = (text1, text2) => {
+
+  const tokens1 = text1.split(" ");
+  const tokens2 = text2.split(" ");
+
+
+  const set1 = new Set(tokens1);
+  const set2 = new Set(tokens2);
+
+
+  const intersection = new Set([...set1].filter((x) => set2.has(x)));
+
+ 
+  const union = new Set([...set1, ...set2]);
+
+
+  const similarity = (intersection.size / union.size) * 100;
+
+
+  return similarity;
+};
+
+//Hesaplama kısmı
+calculateButton.addEventListener("click", () => {
+
+  const text1 = text1Input.value;
+  const text2 = text2Input.value;
+
+  const similarity = calculateSimilarity(text1, text2);
+
+  resultElement.textContent = similarity.toFixed(2);
+});
